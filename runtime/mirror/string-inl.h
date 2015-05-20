663c30257ce0b05451dfb735ef86e80943e0bdf3
arter97 <qkrwngud825@gmail.com>
2015-05-20 23:15:19 +0900

    Implement String.clear() to the runtime
    Samsung Lollipop firmware has a weird java.lang.String.clear() function.
    
    ART needs to know about this to boot properly.
    
    Signed-off-by: arter97 <qkrwngud825@gmail.com>

--- runtime/mirror/string-inl.h
+++ runtime/mirror/string-inl.h
@@ -29,7 +29,7 @@ namespace art {
 namespace mirror {
 
 inline uint32_t String::ClassSize() {
-  uint32_t vtable_entries = Object::kVTableLength + 51;
+  uint32_t vtable_entries = Object::kVTableLength + 52;
   return Class::ComputeClassSize(true, vtable_entries, 1, 1, 2);
 }
 
