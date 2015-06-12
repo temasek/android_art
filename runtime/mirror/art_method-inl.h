97aa9e1d42126b109620a42decb947016453ebed
rovo89 <android@robv.de>
2015-06-12 19:05:01 +0200

    [Xposed] Debugger fixes
    Debuggers need to know about all methods in a class. For methods hooked with
    Xposed, both the hooked (proxy-like) and the original method have to be returned.
    
    Also, the hooked method isn't directly attached to a code item anymore, which
    would otherwise be used to calculate offsets into the method code.
    
    Fixes #1.

--- runtime/mirror/art_method-inl.h
+++ runtime/mirror/art_method-inl.h
@@ -478,6 +478,9 @@ inline const char* ArtMethod::GetName() {
 }
 
 inline const DexFile::CodeItem* ArtMethod::GetCodeItem() {
+  if (UNLIKELY(IsXposedHookedMethod())) {
+    return nullptr;
+  }
   mirror::ArtMethod* method = GetInterfaceMethodIfProxy();
   return method->GetDexFile()->GetCodeItem(method->GetCodeItemOffset());
 }
